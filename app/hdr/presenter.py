5e66b5757456ad7725f3d3b86eb939b6221317dc
Yan Feng <124886155+YFENG-123@users.noreply.github.com>
2025-10-31 19:18:59 +0800

    ✨ feat(app\ | app\hdr | app\jsonp): feat fix (#18)
    feat json_delete_label/tif_draw_label | fix tif_open/tif_save/HdrModel/load_tif

--- app/hdr/presenter.py
+++ app/hdr/presenter.py
@@ -15,7 +15,7 @@ class HdrPresenter:
     def __init__(self, hdr_view: HdrView, hdr_model: HdrModel):
         pass
 
-    def load_hdr(self) -> spectral.envi.SpectralLibrary:
+    def load_hdr(self) -> SpectralLibrary | BilFile | BipFile | BsqFile:
         hdr_path = filedialog.askopenfilename(filetypes=[("HDR", "*.hdr")])
         hdr = spectral.open_image(hdr_path)
         return hdr
@@ -48,7 +48,7 @@ class HdrPresenter:
         # 使用spectral库的envi模块保存HDR/IMG文件对
         spectral.envi.save_image(file_path, hdr_ndarray, metadata=metadata, force=True)
 
-    def save_hdf5(self, hdr) -> None:
+    def save_hdf5(self, hdr: SpectralLibrary | BilFile | BipFile | BsqFile) -> None:
         height = hdr.shape[0]
         width = hdr.shape[1]
         num_channels = hdr.shape[2]
@@ -79,29 +79,28 @@ class HdrPresenter:
 
                 print(f"通道 {i} 已保存")
 
-    def save_hdf5_resize(self, hdr, x1, y1, x2 ,y2) -> None:
-        height = hdr.shape[0]
-        width = hdr.shape[1]
+    def save_hdf5_resize(
+        self, hdr: SpectralLibrary | BilFile | BipFile | BsqFile, x1, y1, x2, y2
+    ) -> None:
         num_channels = hdr.shape[2]
         # 创建HDF5文件（MATLAB v7.3格式）
         with h5py.File("multichannel_data.mat", "w") as file:
             # 创建可扩展的三维数据集（高度 x 宽度 x 通道）
             dset = file.create_dataset(
                 "data",
-                shape=(num_channels, width, height),
-                chunks=(1, width, height),  # 分块大小优化I/O
+                shape=(num_channels, x2 - x1, y2 - y1),
+                chunks=(1, x2 - x1, y2 - y1),  # 分块大小优化I/O
                 dtype=np.uint16,
             )  # 每个通道一个块
             for i in range(0, num_channels):
                 print(f"正在处理通道 {i} ...")
                 print("当前进度：", round(i / num_channels * 100, 2), "%")
-                #只读取指定区域
+                # 只读取指定区域
                 batch_data = hdr.read_band(i)[y1:y2, x1:x2]
                 # 使用内存映射写入当前通道
                 dset[i, :, :] = batch_data.T  # 直接写入新通道
                 del batch_data
                 print(f"通道 {i} 已保存")
-            
 
 
 if __name__ == "__main__":
